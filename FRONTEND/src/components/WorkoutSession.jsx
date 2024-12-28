b11b5fa2b1c1ee92445663ae144b37b2494d5f31
HimashaDewmini <himashadewmini12345@gmail.com>
2024-12-28 12:17:45 +0530

    Initial commit

---
+++ FRONTEND/src/components/WorkoutSession.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const WorkoutSession = () => {
+  return (
+    <section className='workout_session'>
+      <div className='wrapper'>
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quisquam, eaque alias, blanditiis quidem maiores illo unde ipsam!
+        </p>
+        <img src="/img5.jpg" alt="Workout session" />
+      </div>
+      <div className='wrapper'>
+        <h1>FEATURED BOOTCAMS</h1>
+        <p>
+            Lorem ipsum dolor sit awet consectetur adipisicing elit. Corrupti impedit quasi sunt amet rerum accusamus asio eveniet unde?
+
+        </p>
+        <div className="bootcamps">
+            <div>
+                <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa saepa repellendus nemo sit facere ipsam!</p>
+            </div>
+
+            <div>
+                <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa saepa repellendus nemo sit facere ipsam!</p>
+            </div>
+
+            <div>
+                <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa saepa repellendus nemo sit facere ipsam!</p>
+            </div>
+
+            <div>
+                <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa saepa repellendus nemo sit facere ipsam!</p>
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default WorkoutSession;
